b7648e91685114586c60eaaa542932782fdd7f82
Guilherme Abreu <sabreuguilherme@gmail.com>
2020-03-21 01:59:13 -0300

    Update and rename TwoSum.java to java/TwoSum.java

--- java/TwoSum.java
+++ java/TwoSum.java
@@ -9,7 +9,7 @@
 //Because nums[0] + nums[1] = 2 + 7 = 9,
 //return [0, 1].
 
-class Solution {
+class TwoSum {
     public int[] twoSum(int[] nums, int target) {
         Map<Integer, Integer> map = new HashMap<>();
         for (int i =0; i < nums.length ; i++){
